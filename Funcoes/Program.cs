9cd8d2ee80419b64f5075be7da4bfe6e5e7962fc
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 17:47:40 -0300

    Novo

---
+++ Funcoes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            int valor1;
+
+            Console.Write("Inclua o primeiro valor: ");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Inclusa o segundo valor: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado;
+            resultado = valor + valor1;
+
+            Console.WriteLine("O valor da soma foi: " + resultado);
+
+            double resultado1;
+            resultado1 = valor - valor1;
+
+            Console.WriteLine("O valor da divisão foi: " + resultado1);
+
+            Console.ReadLine();
+        }
+    }
+}
